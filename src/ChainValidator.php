297fde82eaa954950b4e5ce00ee03addadd63f43
mkroeders <mkroeders@gmail.com>
2021-11-28 19:36:20 +0100

    Added chain validator

---
+++ src/ChainValidator.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace LessValidator;
+
+use LessValidator\ValidateResult\ValidValidateResult;
+
+/**
+ * @psalm-immutable
+ */
+final class ChainValidator implements Validator
+{
+    /** @var array<Validator> */
+    public array $validators = [];
+
+    /** @param iterable<Validator> $validators */
+    public function __construct(iterable $validators)
+    {
+        foreach ($validators as $validator) {
+            $this->validators[] = $validator;
+        }
+    }
+
+    public function validate(mixed $input): ValidateResult\ValidateResult
+    {
+        foreach ($this->validators as $validator) {
+            $result = $validator->validate($input);
+
+            if (!$result->isValid()) {
+                return $result;
+            }
+        }
+
+        return new ValidValidateResult();
+    }
+}
---
+++ test/ChainValidatorTest.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace LessValidatorTest;
+
+use LessValidator\ChainValidator;
+use LessValidator\ValidateResult\ValidateResult;
+use LessValidator\Validator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \LessValidator\ChainValidator
+ */
+final class ChainValidatorTest extends TestCase
+{
+    public function testValid(): void
+    {
+        $fResult = $this->createMock(ValidateResult::class);
+        $fResult->expects(self::once())->method('isValid')->willReturn(true);
+
+        $f = $this->createMock(Validator::class);
+        $f->expects(self::once())->method('validate')->with('foo')->willReturn($fResult);
+
+        $sResult = $this->createMock(ValidateResult::class);
+        $sResult->expects(self::once())->method('isValid')->willReturn(true);
+
+        $s = $this->createMock(Validator::class);
+        $s->expects(self::once())->method('validate')->with('foo')->willReturn($sResult);
+
+        $validator = new ChainValidator([$f, $s]);
+
+        self::assertTrue($validator->validate('foo')->isValid());
+    }
+
+    public function testInvalid(): void
+    {
+        $fResult = $this->createMock(ValidateResult::class);
+        $fResult->expects(self::once())->method('isValid')->willReturn(true);
+
+        $f = $this->createMock(Validator::class);
+        $f->expects(self::once())->method('validate')->with('foo')->willReturn($fResult);
+
+        $sResult = $this->createMock(ValidateResult::class);
+        $sResult->expects(self::once())->method('isValid')->willReturn(false);
+
+        $s = $this->createMock(Validator::class);
+        $s->expects(self::once())->method('validate')->with('foo')->willReturn($sResult);
+
+        $validator = new ChainValidator([$f, $s]);
+
+        self::assertSame($sResult, $validator->validate('foo'));
+    }
+}
