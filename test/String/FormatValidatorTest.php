0bd367bc0747e685d0ba30f7d899e655e35381b9
mkroeders <mkroeders@gmail.com>
2022-01-15 20:20:48 +0100

    Added string format validator

---
+++ test/String/FormatValidatorTest.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace LessValidatorTest\String;
+
+use LessValidator\String\FormatValidator;
+use LessValidator\ValidateResult\ErrorValidateResult;
+use LessValueObject\String\Format\EmailAddress;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \LessValidator\String\FormatValidator
+ */
+final class FormatValidatorTest extends TestCase
+{
+    public function testValid(): void
+    {
+        $validator = new FormatValidator(EmailAddress::class);
+
+        self::assertTrue($validator->validate('a@b.c')->isValid());
+    }
+
+    public function testInvalid(): void
+    {
+        $validator = new FormatValidator(EmailAddress::class);
+
+        $invalid = $validator->validate('a');
+
+        self::assertFalse($invalid->isValid());
+        self::assertInstanceOf(ErrorValidateResult::class, $invalid);
+        self::assertSame('string.format.emailAddress', $invalid->code);
+    }
+}
