5a5671a16567c00ee21fd7da29dd5274dae287e6
mkroeders <mkroeders@gmail.com>
2024-05-31 15:38:29 +0200

    Expose set values and added helper methods

--- src/Builder/NumericValidatorBuilder.php
+++ src/Builder/NumericValidatorBuilder.php
@@ -16,40 +16,71 @@ final class NumericValidatorBuilder implements ValidatorBuilder
 {
     private bool $onlyIntegers = false;
 
-    private float|int|null $min = null;
+    private float|int|null $minimal = null;
 
-    private float|int|null $max = null;
+    private float|int|null $maximal = null;
 
-    public static function fromBetween(float|int $min, float|int $max): self
+    public static function fromBetween(float|int $minimal, float|int $maximal): self
     {
-        return (new self())->withBetween($min, $max);
+        return (new self())->withBetween($minimal, $maximal);
     }
 
-    public function withBetween(float|int $min, float|int $max): self
+    public function isOnlyIntegers(): bool
+    {
+        return $this->onlyIntegers;
+    }
+
+    public function getMinimal(): float|int|null
+    {
+        return $this->minimal;
+    }
+
+    public function getMaximal(): float|int|null
+    {
+        return $this->maximal;
+    }
+
+    public function withBetween(float|int $minimal, float|int $maximal): self
     {
         $clone = clone $this;
-        $clone->min = $min;
-        $clone->max = $max;
+        $clone->minimal = $minimal;
+        $clone->maximal = $maximal;
 
         return $clone;
     }
 
-    public function withOnlyIntegers(): self
+    public function withMinimum(float|int $minimal): self
     {
         $clone = clone $this;
-        $clone->onlyIntegers = true;
+        $clone->minimal = $minimal;
+
+        return $clone;
+    }
+
+    public function withMaximal(float|int $maximal): self
+    {
+        $clone = clone $this;
+        $clone->maximal = $maximal;
+
+        return $clone;
+    }
+
+    public function withOnlyIntegers(bool $onlyIntegers = true): self
+    {
+        $clone = clone $this;
+        $clone->onlyIntegers = $onlyIntegers;
 
         return $clone;
     }
 
     public function build(): Validator
     {
-        if ($this->min === null) {
-            throw new RuntimeException("No minimum");
+        if ($this->minimal === null) {
+            throw new RuntimeException("No minimal");
         }
 
-        if ($this->max === null) {
-            throw new RuntimeException("No maximum");
+        if ($this->maximal === null) {
+            throw new RuntimeException("No maximal");
         }
 
         return new ChainValidator(
@@ -57,7 +88,7 @@ final class NumericValidatorBuilder implements ValidatorBuilder
                 $this->onlyIntegers
                     ? TypeValidator::integer()
                     : TypeValidator::number(),
-                new BetweenValidator($this->min, $this->max),
+                new BetweenValidator($this->minimal, $this->maximal),
             ],
         );
     }
